dcdcdbefecc9b48b4a699ed82b0cde15b190f98e
Eduardo Orrego <eduardo.orrego@outlook.com>
2026-01-29 11:33:01 -0500

    Remove the explicit access restriction

--- demo-producer/src/main/java/org/xuaxpedia/kafka/demo_producer/DemoProducerApplication.java
+++ demo-producer/src/main/java/org/xuaxpedia/kafka/demo_producer/DemoProducerApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class DemoProducerApplication {
 
-	public static void main(String[] args) {
+	static void main(String[] args) {
 		SpringApplication.run(DemoProducerApplication.class, args);
 	}
 
